dc9261ded7934c6a5705880dbf13ff1764febf0a
YeonwooNuf <jangyw0927@naver.com>
2024-04-16 23:44:49 +0900

    홈만 바꾼 커밋
    홈페이지 내꺼랑 합침

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,12 +1,7 @@
+import 'package:delivery/category/CategorySelect.dart';
+import 'package:delivery/pages/AddressRegisterPage.dart';
+import 'package:delivery/pages/SearchPage.dart';
 import 'package:flutter/material.dart';
-import 'package:delivery/category/Asian.dart';
-import 'package:delivery/category/Chicken.dart';
-import 'package:delivery/category/Chinese.dart';
-import 'package:delivery/category/Japanese.dart';
-import 'package:delivery/category/Korean.dart';
-import 'package:delivery/category/Mexican.dart';
-import 'package:delivery/category/Pizza.dart';
-import 'AddressRegisterPage.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -64,36 +59,45 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ),
               ),
+
               Container(
-                height: 60,
-                color: Colors.white,
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: TextField(
-                          style: TextStyle(color: Colors.black),
-                          decoration: InputDecoration(
-                            hintText: '검색',
-                            hintStyle: TextStyle(color: Colors.grey, fontSize: 20),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(22),
-                            ),
-                            prefixIcon: Icon(Icons.search, color: Colors.black),
+              height: 60,
+              color: Colors.white,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                        onTap: () {
+                          // 검색 필드를 탭했을 때 SearchPage로 이동합니다.
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => SearchPage()),
+                          );
+                        },
+                        style: TextStyle(color: Colors.black),
+                        decoration: InputDecoration(
+                          hintText: '검색',
+                          hintStyle: TextStyle(color: Colors.grey, fontSize: 20),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(22),
                           ),
+                          prefixIcon: Icon(Icons.search, color: Colors.black),
                         ),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
+            ),
             ],
           ),
         ),
       );
     }
 
+//카테고리 정의하는 메서드
     Widget _roundedContainer(String image, String title, VoidCallback onTap) {
       double squareSize = (MediaQuery.of(context).size.width - 60) / 4;
       return Expanded(
@@ -104,7 +108,7 @@ class HomeScreen extends StatelessWidget {
               Container(
                 decoration: BoxDecoration(
                   color: Colors.red,
-                  borderRadius: BorderRadius.circular(30),
+                  borderRadius: BorderRadius.circular(30), // 컨테이너의 모서리를 둥글게 설정
                 ),
                 width: squareSize,
                 height: 50,
@@ -144,56 +148,80 @@ class HomeScreen extends StatelessWidget {
                     children: [
                       Row(
                         children: [
-                          _roundedContainer('assets/images/bibimbap.jpeg', '한식', () {
+                          _roundedContainer('assets/images/bibimbap.jpeg', '한식',
+                              () {
                             Navigator.push(
                               context,
-                              MaterialPageRoute(builder: (context) => Korean()),
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      CategorySelect(CategoryName: '한식')),
                             );
                           }),
                           SizedBox(width: 8),
-                          _roundedContainer('assets/images/sushi.jpeg', '일식', () {
+                          _roundedContainer('assets/images/sushi.jpeg', '일식',
+                              () {
                             Navigator.push(
                               context,
-                              MaterialPageRoute(builder: (context) => Japanese()),
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      CategorySelect(CategoryName: '일식')),
                             );
+                            // 클릭 시 수행할 동작 추가
                           }),
                           SizedBox(width: 8),
-                          _roundedContainer('assets/images/jjajang.jpeg', '중식', () {
+                          _roundedContainer('assets/images/jjajang.jpeg', '중식',
+                              () {
                             Navigator.push(
                               context,
-                              MaterialPageRoute(builder: (context) => Chinese()),
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      CategorySelect(CategoryName: '중식')),
                             );
+                            // 클릭 시 수행할 동작 추가
                           }),
                           SizedBox(width: 8),
-                          _roundedContainer('assets/images/chicken.jpeg', '치킨', () {
+                          _roundedContainer('assets/images/chicken.jpeg', '치킨',
+                              () {
                             Navigator.push(
                               context,
-                              MaterialPageRoute(builder: (context) => Chicken()),
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      CategorySelect(CategoryName: '치킨')),
                             );
+                            // 클릭 시 수행할 동작 추가
                           }),
                         ],
                       ),
                       SizedBox(height: 8),
                       Row(
                         children: [
-                          _roundedContainer('assets/images/pizza.jpeg', '피자', () {
+                          _roundedContainer('assets/images/pizza.jpeg', '피자',
+                              () {
                             Navigator.push(
                               context,
-                              MaterialPageRoute(builder: (context) => Pizza()),
+                              MaterialPageRoute(
+                                  builder: (context) => CategorySelect(
+                                      CategoryName: '피자')), // 클릭 시 수행할 동작 추가
                             );
                           }),
                           SizedBox(width: 8),
-                          _roundedContainer('assets/images/pho.jpeg', '아시아', () {
+                          _roundedContainer('assets/images/pho.jpeg', '아시아',
+                              () {
                             Navigator.push(
                               context,
-                              MaterialPageRoute(builder: (context) => Asian()),
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      CategorySelect(CategoryName: '아시아')),
                             );
                           }),
                           SizedBox(width: 8),
-                          _roundedContainer('assets/images/burrito.jpeg', '멕시칸', () {
+                          _roundedContainer('assets/images/burrito.jpeg', '멕시칸',
+                              () {
                             Navigator.push(
                               context,
-                              MaterialPageRoute(builder: (context) => Mexican()),
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      CategorySelect(CategoryName: '멕시칸')),
                             );
                           }),
                           SizedBox(width: 8),
